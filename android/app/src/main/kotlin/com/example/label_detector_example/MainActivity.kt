6e514fbbf8c7c94ed67d0053e5960f8677aa83a1
Muhammad Sufyan Malik <malikmsufyan@gmail.com>
2023-07-18 22:34:29 +0300

    initial

---
+++ android/app/src/main/kotlin/com/example/label_detector_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.label_detector_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Utilities/CustomDesigns/CustomCard.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class CustomCard extends StatelessWidget {
+  final String _label;
+  final Widget _viewPage;
+  final bool featureCompleted;
+
+  const CustomCard(this._label, this._viewPage, {this.featureCompleted = true});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      margin: EdgeInsets.only(bottom: 10),
+      child: ListTile(
+        tileColor: Theme.of(context).primaryColor,
+        title: Text(
+          _label,
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        onTap: () {
+          if (!featureCompleted) {
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                content:
+                    const Text('This feature has not been implemented yet')));
+          } else {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => _viewPage));
+          }
+        },
+      ),
+    );
+  }
+}
